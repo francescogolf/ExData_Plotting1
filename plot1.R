a2f5c794bc32fe1aa07bc301b8afd14d928f6d0c
Francesco Golfieri <francesco.golfieri@gmail.com>
2016-10-01 10:41:35 +0200

    Plot1 Script

--- plot1.R
+++ plot1.R
@@ -2,7 +2,6 @@
 hpc <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 ##select only 1,2 February
 subSetData <- data[hpc$Date %in% c("1/2/2007","2/2/2007") ,]
-#str(subSetData)
 ##convert Global active power to numeric
 globalActivePower <- as.numeric(subSetData$Global_active_power)
 ##create plot and saves to plot1.png
